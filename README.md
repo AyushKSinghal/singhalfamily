1691ad74aabaa684faad55fda8324b3238adefb0
Ayush Singhal <ayushksinghal@gmail.com>
2020-03-31 22:34:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# singhalfamily
+# react-dhkmtj
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/singhalfamily)
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-dhkmtj)
---
+++ overview-data.js
@@ -0,0 +1,199 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Local data for diagram layouts
+ */
+exports.data = [
+    {
+        'Id': 30, 
+        'Name': 'Ishaar Das 1', 
+        'Designation': 'Businessman',
+        'MarriedTo': '',
+        'SpouseCity': '',
+        'MarriageDate': '',
+        'JamanTithi': '',
+        'Description': '',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0'
+    },
+    {
+        'Id': 31, 'Name': 'Vinod Kumar Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#68C2DE', 'ReportingPerson':30
+    },
+    {
+        'Id': 32, 'Name': 'Vinay Singhal', 'Designation': 'IBM Risk Manager',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 31
+    },
+    {
+        'Id': 33, 'Name': 'Vineet Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#68C2DE', 'ReportingPerson': 31
+    },
+    {
+        'Id': 34, 'Name': 'Vipin Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'true',
+        'RatingColor': '#D46E89', 'ReportingPerson': 31
+    },
+    {
+        'Id': 35, 'Name': 'Princy Singhal', 'Designation': 'Married to Paresh Ji - Pilibhit',
+        'ImageUrl': 'https://cdn2.iconfinder.com/data/icons/business-and-finance-related-hand-gestures/256/face_female_blank_user_avatar_mannequin-512.png', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 31
+    },
+    {
+        'Id': 61, 'Name': 'Praveen Kumar Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#68C2DE', 'ReportingPerson': 30
+    },
+    {
+        'Id': 62, 'Name': 'Naval Singhal', 
+        'Designation': 'Businessman - MP',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 61
+    },
+    {
+        'Id': 63, 'Name': 'Rinkal Dev', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#68C2DE', 'ReportingPerson': 61
+    },
+    {
+        'Id': 64, 'Name': 'Ankit Singhal', 'Designation': 'Indian Army :)',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'true',
+        'RatingColor': '#D46E89', 'ReportingPerson': 61
+    },
+    {
+        'Id':65, 'Name': 'Neetu Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'true',
+        'RatingColor': '#D46E89', 'ReportingPerson': 61
+    },
+    {
+        'Id':66, 'Name': 'Sumit Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'true',
+        'RatingColor': '#D46E89', 'ReportingPerson': 61
+    },
+    {
+        'Id': 67, 'Name': 'Shipra Didi', 'Designation': 'Married in Chilkana',
+        'ImageUrl': 'https://cdn2.iconfinder.com/data/icons/business-and-finance-related-hand-gestures/256/face_female_blank_user_avatar_mannequin-512.png', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 61
+    },
+    {
+        'Id': 101, 'Name': 'Pushottam Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 30
+    },
+    {
+        'Id': 162, 'Name': 'Ankur Singhal', 'Designation': 'Techno Functional Manager',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 101
+    },
+    {
+        'Id': 163, 'Name': 'Shradha Gupta', 'Designation': 'Homemaker',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 101
+    },
+    {
+        'Id': 164, 'Name': 'Sakshi *', 'Designation': 'Homemaker',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 101
+    },
+    {
+        'Id': 131, 'Name': 'Pawan Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 30
+    },
+    {
+        'Id': 132, 'Name': 'Rahul Singhal', 'Designation': 'Techno Functional Manager',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 131
+    },
+    {
+        'Id': 133, 'Name': 'Vipul Singhal', 'Designation': 'Techno Functional Manager',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 131
+    },
+    {
+        'Id': 134, 'Name': 'Vibhore Singhal', 'Designation': 'Software Engineer',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 131
+    },
+    {
+        'Id': 135, 'Name': 'Akansha *', 'Designation': 'Software Engineer',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 131
+    },
+    {
+        'Id': 161, 'Name': 'Yogesh Kumar Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 30
+    },
+    {
+        'Id': 162, 'Name': 'Arpit Singhal', 'Designation': 'Techno Functional Engineer',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 161
+    },
+    {
+        'Id': 163, 'Name': 'Ayush Singhal', 'Designation': 'Software Engineer',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 161
+    },
+    {
+        'Id': 164, 'Name': 'Achin Singhal', 'Designation': 'Software Engineer',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 161
+    },
+    {
+        'Id': 191, 'Name': 'Mukesh Kumar Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 30
+    },
+    {
+        'Id': 192, 'Name': 'Divik Singhal', 'Designation': 'Software Analyst',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 191
+    },
+    {
+        'Id': 193, 'Name': 'Deepanshi Singhal', 'Designation': 'Manager',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 191
+    },
+    {
+        'Id': 221, 'Name': 'Brij Mohan', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 30
+    },
+    {
+        'Id': 222, 'Name': 'Vishu *', 'Designation': 'Housewife',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 221
+    },
+    {
+        'Id': 223, 'Name': '*Gulgul', 'Designation': 'Student',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 221
+    },
+    {
+        'Id': 224, 'Name': '*Chini', 'Designation': 'Student',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 221
+    },
+    {
+        'Id': 225, 'Name': 'Sanskar Singhal', 'Designation': 'Student',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 221
+    },
+    {
+        'Id': 251, 'Name': 'Komal Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 30
+    },
+    {
+        'Id': 252, 'Name': 'Vikas Singhal', 'Designation': 'Software Engineer',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 251
+    },
+    {
+        'Id': 253, 'Name': 'Ashish Singhal', 'Designation': 'Businessman',
+        'ImageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREmFx6Ff6QaAYkenrTeT5v29yCVjycSXOxkfU-mh9tRJoxMfd0', 'IsExpand': 'false',
+        'RatingColor': '#93B85A', 'ReportingPerson': 251
+    },
+];
